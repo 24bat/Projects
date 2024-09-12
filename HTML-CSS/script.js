e2b05f79aa0740deecb64ce61fa66895118e6462
Hezekiah <youaretoougly01@gmail.com>
2024-09-12 11:56:26 +0000

    Add files via upload
    This is a one page portfolio codes

---
+++ HTML-CSS/script.js
@@ -0,0 +1,8 @@
+
+const menuIcon = document.getElementById('menu-icon'); 
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x'); 
+    navbar.classList.toggle('active');
+}
